f9ec7c70ea2e35c74e9c70ec3152d197411125d1
IamNoldor <82669778+IamNoldor@users.noreply.github.com>
2023-05-08 20:13:19 +0300

    v.1.2

--- TikTok-Chat-Reader-main/public/app.js
+++ TikTok-Chat-Reader-main/public/app.js
@@ -140,6 +140,9 @@ function addGiftItem(data) {
     }
 
     container.stop();
+    container.animate({
+        scrollTop: container[0].scrollHeight
+    }, 800);
 }
 
 
--- TikTok-Chat-Reader-main/public/changeChecker.js
+++ TikTok-Chat-Reader-main/public/changeChecker.js
@@ -1,18 +1,10 @@
 let count_of_gifts = 1
 document.addEventListener("DOMNodeInserted", (evt) => {
     if (evt.target.className == "giftitem") {
+        evt.target.style.background = "rgba(255,202,4,0.75)"
         getGiftInfo(evt.target)
     }
 })
-
-// document.addEventListener("DOMSubtreeModified", (e) => {
-//     let gift_container = document.getElementsByClassName("giftcontainer")[0]
-//     if (e.target.className == "giftcontainer" && count_of_gifts <= gift_container.childElementCount) {
-//         count_of_gifts++
-//         doSound()
-//         getGiftInfo()
-//     }
-// }, false)
 
 function getGiftInfo(evt_target) {
     let nickname = evt_target.children[1].children[0].children[0].text
@@ -20,9 +12,7 @@ function getGiftInfo(evt_target) {
     rep_nick = rep_nick.replace(".", "")
     //let gift_price = evt_target.children[1].children[1].children[0].children[0].children[1].children[0].textContent
     doSound()
-    setTimeout(() => {
-        speak(rep_nick)
-    }, 500)
+    speak(rep_nick, evt_target)
 }
 
 function doSound() {
@@ -32,9 +22,22 @@ function doSound() {
     audio.autoplay = true
 }
 
-function speak(nickname) {
-    let speech = window.speechSynthesis
-    let text = new SpeechSynthesisUtterance("Дякую за подарунок " + nickname)
-    text.volume = 2
-    speech.speak(text)
+function speak(nickname, evt_target) {
+    // evt_target.style.backgroundColor = "green"
+    // let speech = window.speechSynthesis
+    // let text = new SpeechSynthesisUtterance("Дякую за подарунок " + nickname)
+    //
+    // speech.speak(text)
+    // speech.onend = () => {
+    //     evt_target.style.backgroundColor = "transparent"
+    // }
+    let speech = new SpeechSynthesisUtterance()
+    speech.text = "Дякую за подарунок " + nickname
+    speech.onstart = () => {
+        evt_target.style.background = "rgba(143,194,80,0.71)"
+    }
+    speech.onend = () => {
+        evt_target.style.backgroundColor = "transparent"
+    }
+    speechSynthesis.speak(speech)
 }
